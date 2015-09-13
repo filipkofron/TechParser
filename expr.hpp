6754ffb60dd3c8a5146bb650e67f38201406a5cb
Filip Kofron <filip.kofron.cz@gmail.com>
2015-09-13 14:12:54 +0200

    Added few basic expressions.

--- expr.hpp
+++ expr.hpp
@@ -1,7 +1,33 @@
+#ifndef EXPR_HPP
+#define EXPR_HPP
+
+#include <QString>
+
 class Expr
 {
 protected:
-  std::string content;
 public:
-  const std::string &GetContent() const;
+  virtual const QString ToString() const = 0;
+  virtual Expr *Simplify() { }
 };
+
+class ExprString
+{
+protected:
+  QString _content;
+public:
+  ExprString(const QString &content) : _content(content) { }
+  virtual const QString ToString() const override { return _content; }
+  virtual Expr *Simplify() { return new ExprString(_content); }
+};
+
+class ExprParenthesis
+{
+protected:
+  Expr *_subExpr;
+public:
+  virtual const QString ToString() const override { return subExpr->ToString(); }
+  virtual void Simplify() override { _subExpr->Simplify(); }
+};
+
+#endif // EXPR_HPP
